fb5d96d366ff776db13ddb0c6075b15428271eb3
amine aqebli <aqebli36@gmail.com>
2022-07-19 14:29:18 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div className="actions">
+          <button className="btn">Delete </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
